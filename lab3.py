d30f2a7bac718350216092c731c7e9127ea71c34
Рома Безделов <roma37557@gmail.com>
2024-11-19 22:11:26 +0700

    Настройки отображения

--- lab3.py
+++ lab3.py
@@ -69,3 +69,35 @@ def pay():
 def success():
     global price
     return render_template('/lab3/success.html', price=price)
+
+
+@lab3.route('/lab3/settings')
+def settings():
+    color = request.args.get('color')
+    background_color = request.args.get('background_color')
+    font_size = request.args.get('font_size')
+    text_align = request.args.get('text_align')
+    if color or background_color or font_size or text_align:
+        resp = make_response(render_template(
+            'lab3/settings.html',
+            color=color,
+            background_color=background_color,
+            font_size=font_size,
+        ))
+        if color:
+            resp.set_cookie('color', color)
+        if background_color:
+            resp.set_cookie('background_color', background_color)
+        if font_size:
+            resp.set_cookie('font_size', font_size)
+        return resp
+    color = request.cookies.get('color')
+    background_color = request.cookies.get('background_color')
+    font_size = request.cookies.get('font_size')
+    resp = make_response(render_template(
+        'lab3/settings.html',
+        color=color,
+        background_color=background_color,
+        font_size=font_size,
+    ))
+    return resp
