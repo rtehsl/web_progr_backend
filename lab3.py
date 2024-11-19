b7e9e2401346495e5bc94b12755bda09ba978d2a
Рома Безделов <roma37557@gmail.com>
2024-11-19 21:49:19 +0700

    Заказ напитка

--- lab3.py
+++ lab3.py
@@ -38,3 +38,34 @@ def form1():
         errors['age'] = 'Заполните поле!'
     sex = request.args.get('sex')
     return render_template('lab3/form1.html', user=user, age=age, sex=sex, errors=errors)
+
+
+@lab3.route("/lab3/order")
+def order():
+    return render_template('lab3/order.html')
+
+
+price = 0
+@lab3.route('/lab3/pay')
+def pay():
+    global price
+    drink = request.args.get('drink')
+    if drink == 'cofee':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+    
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price += 10
+
+    return render_template('lab3/pay.html', price=price)
+
+
+@lab3.route('/lab3/success')
+def success():
+    global price
+    return render_template('/lab3/success.html', price=price)
